b793c1b9d16b1b9d1b02b0628b243e29f34837a4
kubo.financiero <jesus.garcia@kubofinanciero.com>
2019-07-11 17:59:20 -0500

    Se agregó el menú y footer para carga dinamica

--- js/javascript-basico.js
+++ js/javascript-basico.js
@@ -1,91 +1,131 @@
-WebFontConfig = {google: { families: ['Muli:300,400,700'] }};(function() {var wf = document.createElement('script'); wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js'; wf.type = 'text/javascript'; wf.async = 'true'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wf, s);})();
+WebFontConfig = { google: { families: ['Muli:300,400,700'] } };
+(function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
 
-$("document").ready(function () {
+$("document").ready(function() {
 
-  setHeaderHeight();
-
-  $("#hamburger-menu").on("click", function(){
-    if($("#header-menu").hasClass("open")){
-      $("#header-menu").removeClass("open").addClass("close")
-    } else {
-      $("#header-menu").removeClass("close").addClass("open")
-    }
-  });
-
-
-  /***  MODAL DIALOG  ***/
-  $(".link-modal-dialog").on("click", function(){
-    var stringId = $(this).attr("data-content-modal-dialog");
-    var content = $("<div>").addClass(stringId).html($("#"+stringId).html());
-
-    $("body").css("overflow", "hidden");
-    $("#modal-dialog .dinamic-content").html(content);
-
-    centerModal();
-    $("#modal-dialog").removeClass("close").addClass("open");
-  });
-
-  $("#modal-dialog .close-modal, #modal-dialog").on("click",function(e){
-    var t = e.target || e.srcElement;
-    if($(t).is("#modal-dialog") || $(t).hasClass("close-modal")){
-      $("body").css("overflow", "initial");
-      $("#modal-dialog").removeClass("open").addClass("close");
-      //$("#modal-dialog .dinamic-content").html("");
-    }
-  });
-  /***   MODAL DIALOG   ***/
-
-  $(window).resize(function(){
     setHeaderHeight();
-    centerModal();
-  });
+
+    $("#hamburger-menu").on("click", function() {
+        if ($("#header-menu").hasClass("open")) {
+            $("#header-menu").removeClass("open").addClass("close")
+        } else {
+            $("#header-menu").removeClass("close").addClass("open")
+        }
+    });
+
+
+
+    /***  MODAL DIALOG  ***/
+    $(".link-modal-dialog").on("click", function() {
+        var stringId = $(this).attr("data-content-modal-dialog");
+        var content = $("<div>").addClass(stringId).html($("#" + stringId).html());
+
+        $("body").css("overflow", "hidden");
+        $("#modal-dialog .dinamic-content").html(content);
+
+        centerModal();
+        $("#modal-dialog").removeClass("close").addClass("open");
+    });
+
+    $("#modal-dialog .close-modal, #modal-dialog").on("click", function(e) {
+        var t = e.target || e.srcElement;
+        if ($(t).is("#modal-dialog") || $(t).hasClass("close-modal")) {
+            $("body").css("overflow", "initial");
+            $("#modal-dialog").removeClass("open").addClass("close");
+            //$("#modal-dialog .dinamic-content").html("");
+        }
+    });
+    /***   MODAL DIALOG   ***/
+
+    $(window).resize(function() {
+        setHeaderHeight();
+        centerModal();
+    });
 
 });
 
 
-function setHeaderHeight(){
-  var headerTopHeight = $(window).width() > 768? 100 : 38;
-  var headerHeight = $(window).height() - headerTopHeight;
-  $("#header, #header .container-center").height(headerHeight + "px");
+function setHeaderHeight() {
+    var headerTopHeight = $(window).width() > 768 ? 100 : 38;
+    var headerHeight = $(window).height() - headerTopHeight;
+    $("#header, #header .container-center").height(headerHeight + "px");
 }
 
-function centerModal(){
-  setModalWidth();
+function centerModal() {
+    setModalWidth();
 
-  var widthWindow = $(window).width();
-  var heightWindow = $(window).height();
+    var widthWindow = $(window).width();
+    var heightWindow = $(window).height();
 
-  var widthContainer = $("#modal-dialog .container").width()/2;
-  var heightContainer = $("#modal-dialog .container").height();
+    var widthContainer = $("#modal-dialog .container").width() / 2;
+    var heightContainer = $("#modal-dialog .container").height();
 
-  var topPosition = parseInt((heightWindow/2) - (heightContainer/2));
+    var topPosition = parseInt((heightWindow / 2) - (heightContainer / 2));
 
-  if(widthWindow > 768){
+    if (widthWindow > 768) {
 
-    var leftPosition = parseInt(widthWindow/2 - widthContainer);
-    $("#modal-dialog > .container").css({
-      "margin-left": leftPosition + "px",
-      "margin-top": topPosition + "px"
-    });
-  } else {
-    if(heightContainer >= heightWindow){
-      topPosition = 10;
+        var leftPosition = parseInt(widthWindow / 2 - widthContainer);
+        $("#modal-dialog > .container").css({
+            "margin-left": leftPosition + "px",
+            "margin-top": topPosition + "px"
+        });
+    } else {
+        if (heightContainer >= heightWindow) {
+            topPosition = 10;
+        }
+
+        $("#modal-dialog > .container").css({
+            "margin-top": topPosition + "px",
+            "margin-left": "10px"
+        });
     }
-
-    $("#modal-dialog > .container").css({
-      "margin-top": topPosition + "px",
-      "margin-left": "10px"
-    });
-  }
 }
 
-function setModalWidth(){
-  var paddingContainer = 100;
-  var widthContent = $("#modal-dialog .dinamic-content > div").width() + paddingContainer;
+function setModalWidth() {
+    var paddingContainer = 100;
+    var widthContent = $("#modal-dialog .dinamic-content > div").width() + paddingContainer;
 
-  if($(window).width() > 768){
-    $("#modal-dialog .container").width(widthContent+"px");
-  } else {
-    $("#modal-dialog .container").css("width", "initial");
-  }
+    if ($(window).width() > 768) {
+        $("#modal-dialog .container").width(widthContent + "px");
+    } else {
+        $("#modal-dialog .container").css("width", "initial");
+    }
+}
+
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+
+        // Check if the XMLHttpRequest object has a "withCredentials" property.
+        // "withCredentials" only exists on XMLHTTPRequest2 objects.
+        xhr.open(method, url, true);
+
+    } else if (typeof XDomainRequest != "undefined") {
+
+        // Otherwise, check if XDomainRequest.
+        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+
+    } else {
+
+        // Otherwise, CORS is not supported by the browser.
+        xhr = null;
+
+    }
+    return xhr;
 }
